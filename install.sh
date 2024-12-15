4b41f7c5c9a6b754a6d6f3dda924a802c786bbfe
dough10 <doughten.jimi@gmail.com>
2024-12-15 17:35:36 -0600

    actualy works now

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ set -e
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install cifs-utils -y
+sudo apt-get install cifs-utils samba-client -y
 
 echo "Run autoremove? (y/n)"
 read -r auto
-if [[ "$auto" =~ ^[yY]$ ]]; then
+if [ "$auto" = "y" ] || [ "$auto" = "Y" ]; then
   sudo apt-get autoremove -y
 fi
 
@@ -16,7 +16,10 @@ echo "Enter SMB Username: "
 read -r uname
 
 echo "Enter SMB Password: "
-read -rs pass
+stty -echo
+read -r pass
+stty echo
+echo
 
 credentials_file="$HOME/.smbcredentials"
 
@@ -33,7 +36,7 @@ echo "Enter file share address (e.g., //192.168.1.100/Music): "
 read -r server
 
 echo "Checking if the share $server is accessible..."
-smbclient -L "$server" -A "$credentials_file" > /dev/null 2>&1
+smbclient -L "$server" -A "$credentials_file" > smbclient_output.txt 2>&1
 
 if [ $? -eq 0 ]; then
   echo "Server $server is accessible. Proceeding."
@@ -41,23 +44,25 @@ if [ $? -eq 0 ]; then
   echo "Enter the share folder name (e.g., 'nas'): "
   read -r folder
   
-  echo "Is /media/$folder the correct location? (y/n): "
+  echo "Is /media/$USER/$folder the correct location? (y/n): "
   read -r confirm_folder
 
-  while [[ "$confirm_folder" != "y" ]] && [[ "$confirm_folder" != "Y" ]]; do
+  while [ "$confirm_folder" != "y" ] && [ "$confirm_folder" != "Y" ]; do
     echo "Please enter the correct share folder: "
     read -r folder
-    echo "Is /media/$folder the correct location? (y/n): "
+    echo "Is /media/$USER/$folder the correct location? (y/n): "
     read -r confirm_folder
   done
 
+  mkdir -pv /media/$USER/$folder
+
   echo "Adding share to /etc/fstab..."
-  echo "//${server}/${folder} /media/${folder} cifs credentials=$HOME/.smbcredentials,uid=$USER,nofail 0 0" | sudo tee -a /etc/fstab > /dev/null
+  echo "${server} /media/$USER/$folder  cifs credentials=$HOME/.smbcredentials,uid=$USER,nofail 0 0" | sudo tee -a /etc/fstab > /dev/null
 
   echo "Share successfully added to /etc/fstab."
 
   if [ -d "$HOME/Desktop" ]; then
-    echo -e "[Desktop Entry]\nVersion=1.0\nName=Mount Share\nExec=sudo mount /media/${folder}\nIcon=folder\nTerminal=true\nType=Application\nCategories=Utility;" | tee -a "$HOME/Desktop/mount_share.desktop" > /dev/null
+    echo "[Desktop Entry]\nVersion=1.0\nName=Mount Share\nExec=sudo mount /media/$USER/$folder \nIcon=folder\nTerminal=true\nType=Application\nCategories=Utility;" | tee -a "$HOME/Desktop/mount_share.desktop" > /dev/null
 
     chmod +x "$HOME/Desktop/mount_share.desktop"
     echo "Mount share shortcut has been created on your Desktop."
@@ -65,7 +70,7 @@ if [ $? -eq 0 ]; then
 
   echo "Do you want to restart now? (y/n): "
   read -r restart
-  if [[ "$restart" =~ ^[yY]$ ]]; then
+  if [ "$restart" = "y" ] || [ "$restart" = "Y" ]; then
     sudo reboot
   fi
 else
